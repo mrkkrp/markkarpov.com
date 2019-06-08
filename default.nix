771b3671f62fe145930e41ce8c86737732afb03f
mrkkrp <markkarpov92@gmail.com>
2019-06-08 13:49:10 +0200

    Build actual HTML files using Nix
    A bit nicer.

--- default.nix
+++ default.nix
@@ -1,28 +1,80 @@
 let
   compiler = "ghc864";
   pkgs = import ./nix/nixpkgs;
-  sourceRegex = [
+  appSourceRegex = [
     "^app.*$"
-    "^markkarpov-com.cabal$"
-    "^README.md$"
+    "^markkarpov-com\.cabal$"
+    "^README\.md$"
+  ];
+  siteSourceRegex = [
+    "^attachment.*$"
+    "^config.*$"
+    "^megaparsec.*$"
+    "^notes.*$"
+    "^post.*$"
+    "^raw.*$"
+    "^resume$" "^resume/resume\.md$"
+    "^static.*$"
+    "^templates.*$"
+    "^tutorial.*$"
+    "^about\.md$"
   ];
   haskellPackages = pkgs.haskell.packages.${compiler}.override
     { overrides = (self: super:
         super //
-        { "markkarpov-com" = super.callCabal2nix "markkarpov-com" (pkgs.lib.sourceByRegex ./. sourceRegex) {};
+        { "markkarpov-com" = super.callCabal2nix "markkarpov-com" (pkgs.lib.sourceByRegex ./. appSourceRegex) {};
           "ghc-syntax-highlighter" = pkgs.haskell.lib.overrideCabal super.ghc-syntax-highlighter (drv: {
             version = "0.0.4.0";
             sha256 = "1kw1h7n4ydn1klzll24nwwg405j23wry9hg8g96vba51vah0wc47";
           });
         });
     };
-in if pkgs.lib.inNixShell
+  html5validator = import ./nix/html5validator;
+  texlive = import ./nix/texlive-custom;
+  app =
+    if pkgs.lib.inNixShell
     then haskellPackages.shellFor
       { packages = (ps: [ ps.markkarpov-com ]);
         buildInputs = [
           pkgs.cabal-install
-          pkgs.glibcLocales
         ];
-        LANG = "en_US.UTF-8";
       }
-    else haskellPackages.markkarpov-com
+    else haskellPackages.markkarpov-com;
+  resume = pkgs.stdenv.mkDerivation {
+    name = "resume-in-pdf";
+    buildInputs = [
+      texlive
+    ];
+    unpackPhase = "true";
+    buildPhase = ''
+      pdflatex ${./resume/resume.tex}
+    '';
+    installPhase = ''
+      mkdir "$out"
+      cp *-resume.pdf $out/resume.pdf
+    '';
+  };
+  site = pkgs.stdenv.mkDerivation {
+    name = "mk-com";
+    buildInputs = [
+      haskellPackages.markkarpov-com
+      html5validator
+      pkgs.glibcLocales
+      pkgs.openjdk
+      resume
+    ];
+    LANG = "en_US.UTF-8";
+    src = pkgs.lib.sourceByRegex ./. siteSourceRegex;
+    buildPhase = ''
+      cp ${resume}/resume.pdf resume/resume.pdf
+      mk-com
+      html5validator --root _build/ --show-warnings
+    '';
+    installPhase = ''
+      mkdir "$out"
+      cp -r _build "$out/_build"
+    '';
+  };
+in {
+   inherit app resume site;
+}

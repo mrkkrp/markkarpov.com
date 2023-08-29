e875641e65ea7708487bc59bfff79e2d78f1e57a
Mark Karpov <markkarpov92@gmail.com>
2023-08-29 21:49:45 +0200

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/be9e3762e719211368d186f547f847737baad720' (2022-11-28)
      → 'github:NixOS/nixpkgs/cddebdb60de376c1bdb7a4e6ee3d98355453fe56' (2023-08-27)

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
         system = "x86_64-linux";
         config.allowUnfree = true;
       };
-      compiler = "ghc943";
+      compiler = "ghc945";
       appSourceRegex = [
         "^app.*$"
         "^markkarpov-com\.cabal$"
@@ -39,7 +39,6 @@
             "markkarpov-com" = super.callCabal2nix "markkarpov-com"
               (pkgs.lib.sourceByRegex ./. appSourceRegex)
               { };
-            "ghc-syntax-highlighter" = super.ghc-syntax-highlighter_0_0_9_0;
           });
         };
       html5validator = with pkgs.python39Packages; buildPythonPackage rec {
